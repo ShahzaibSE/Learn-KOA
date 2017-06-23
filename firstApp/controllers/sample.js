77cb35689634b5d73edfda20d5a2d35745313eb8
ShahzaibSE <shahzaibanalyst@outlook.com>
2017-06-23 21:37:13 +0500

    First KOA sample app

---
+++ firstApp/controllers/sample.js
@@ -0,0 +1,11 @@
+/**
+ * Created by shahzaibnoor on 20/06/2017.
+ */
+
+'use strict';
+const views = require('co-views');
+const parse = require('co-body');
+
+module.exports.sampleResponse = function* (){
+  this.body = yield { message: "Congratulations! on your first KOA response" };
+}
